cc5bd2abdbc8733faf80134caf96916129ea438b
Colby Austin Brown <colbyaustinbrown@gmail.com>
2022-08-09 19:06:24 -0700

    Fix poultry solution

--- mock-1/solutions/poultry.py
+++ mock-1/solutions/poultry.py
@@ -4,13 +4,15 @@ prices = dict()
 
 for i in range(0, n):
     line = input().split(" ")
-    name = line[:-1][0]
+    name = line[:-1]
+    fullname = ""
+    for n in name:
+        fullname = fullname + " " + n
     price = float(line[-1:][0][1:])
 
-    if not (name in prices):
-        prices[name] = 0
-    else:
-        prices[name] = prices[name] + price
+    if not (fullname in prices):
+        prices[fullname] = 0
+    prices[fullname] = prices[fullname] + price
 
 keys = list(prices.keys())
 keys.sort()
